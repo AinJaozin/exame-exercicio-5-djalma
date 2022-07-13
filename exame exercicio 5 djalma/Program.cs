7d1be910e585f7b1cbf6d9032efa2869cde50ceb
55119 <55119@DESKTOP-3E6FVKU>
2022-07-13 19:26:44 -0300

    Adicionar arquivos de projeto.

---
+++ exame exercicio 5 djalma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exame_exercicio_5_djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i * 2);
+            }
+            Console.ReadLine();
+
+            //****************************************************************************************************************************************
+
+            Console.ReadKey();
+
+        }
+    }
+}
+        
+    
